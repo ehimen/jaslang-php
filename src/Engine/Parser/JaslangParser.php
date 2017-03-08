b6531201b55a4c77a1716ce81df906cc4bccbd03
Ben Gibson <ben.gibson@edigitalresearch.com>
2017-03-08 18:51:26 +0000

    Support state term on last line

--- src/Engine/Parser/JaslangParser.php
+++ src/Engine/Parser/JaslangParser.php
@@ -258,6 +258,7 @@ class JaslangParser implements Parser
             ->accept($operator)
             ->accept($identifier)
             ->accept($blockClose)
+            ->accept($stateTerm)
         ;
         
         return $builder->build();
